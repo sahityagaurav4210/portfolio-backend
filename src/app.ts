8ce1d56a5709c772c5630ddc6c6dc339cc17a849
sahityagaurav4210 <sahityagaurav.41125@gmail.com>
2024-07-31 14:28:38 +0530

    fix: cors origin
    cors origin url

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.set('trust proxy', true);
 app.use(express.json({ limit: '12kb' }));
 app.use(express.urlencoded({ extended: true, limit: '6kb' }));
 app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
-app.use(cors({ origin: process.env.ACCEPTED_CLIENTS, credentials: true }));
+app.use(cors({ origin: process.env.ALLOWED_HOSTS, credentials: true }));
 app.use(cookieParser());
 
 app.use('/api/v1', route);
