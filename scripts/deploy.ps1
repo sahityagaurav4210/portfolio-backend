e96c7bb9e95ccfe04bb54a4b79962af16a28ad6f
Gaurav <165876233+gauravrmt@users.noreply.github.com>
2025-01-21 15:17:47 +0530

    Update deploy.ps1

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,9 +1,13 @@
-param([string]$url, [string]$code)
+param([string]$url)
 Write-Output "Preparing the image..."
 
 docker build -t sgauravdev/portfolio-builder-backend:latest .
 docker push sgauravdev/portfolio-builder-backend:latest
 
+$secret = Read-Host "Enter password" -AsSecureString
 Write-Output "Deploying the app, please wait..."
+$plainPwd = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+  [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secret)
+)
 
-caprover deploy -h "$url" -p "$code" -i sgauravdev/portfolio-builder-backend --appName "portfolio-backend"
+caprover deploy -h "$url" -p "$plainPwd" -i sgauravdev/portfolio-builder-backend --appName "portfolio-backend"
