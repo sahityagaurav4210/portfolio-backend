5a12f6e798e29152ef4c98097d172c2427596479
Gaurav <165876233+gauravrmt@users.noreply.github.com>
2025-02-18 17:54:57 +0530

    feat: added view-profile api

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -24,8 +24,31 @@ class UserController {
       return response.status(HTTP_STATUS_CODES.UPDATED).json(reply);
     } else {
       reply.STATUS = Status.ERROR;
-      reply.MESSAGE = 'An error occured, please try again after sometime';
+      reply.MESSAGE = 'An error occurred, please try again after sometime';
       reply.ENTRY_BY = authenticatedUser.phone;
+
+      return response.status(HTTP_STATUS_CODES.SERVER_ERR).json(reply);
+    }
+  }
+
+  @HandleException()
+  public static async viewProfile(request: CustomReq, response: Response): Promise<Response> {
+    const { authenticatedUser } = request;
+    const reply = new ApiResponse();
+
+    const profile = await User.findById(authenticatedUser?._id);
+
+    if (profile) {
+      reply.STATUS = Status.SUCCESS;
+      reply.MESSAGE = 'Profile fetched successfully';
+      reply.DATA = profile;
+      reply.ENTRY_BY = authenticatedUser.phone || request.ip || '0.0.0.0';
+
+      return response.status(HTTP_STATUS_CODES.UPDATED).json(reply);
+    } else {
+      reply.STATUS = Status.ERROR;
+      reply.MESSAGE = 'An error occurred, please try again after sometime';
+      reply.ENTRY_BY = authenticatedUser.phone || request.ip || '0.0.0.0';
 
       return response.status(HTTP_STATUS_CODES.SERVER_ERR).json(reply);
     }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,5 +5,6 @@ import Controller from '../controllers';
 const route = Router();
 
 route.put('/edit-profile', Middleware.checkIfAuthenticated, Controller.user().editProfile);
+route.get('/profile', Middleware.checkIfAuthenticated, Controller.user().viewProfile);
 
 export default route;
