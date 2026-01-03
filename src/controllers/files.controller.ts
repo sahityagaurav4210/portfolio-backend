cd6d030b7efe67200b63ce896491a50102c03286
Gaurav Sahitya <works.sahitya@gmail.com>
2026-01-03 19:22:52 +0530

    fix: Files class import error

--- src/controllers/files.controller.ts
+++ src/controllers/files.controller.ts
@@ -1,20 +1,20 @@
-import * as path from 'path';
+import * as path from 'node:path';
 import { Request, Response } from 'express';
 import { HandleException } from '../decorators/exception.decorator';
-import { Files, parseQsAsBoolean } from '../helpers';
+import { parseQsAsBoolean } from '../helpers';
+import Files from '@helpers/files.helpers';
 import { getObjectAsBlob } from '@helpers/aws.helpers';
 import { ApiResponse, HTTP_STATUS_CODES, Status } from '@api/index';
 
 import { Files as FileModel } from '@models/files.model';
 import { CustomReq } from '@interfaces/index';
 import { User } from '@models/users.model';
-import Packages from '@packages/index';
 class FilesController {
   @HandleException()
   public static async downloadCV(request: Request, response: Response) {
     const reply = new ApiResponse();
     const token = request.query.token;
-    const record = await FileModel.findOne({ token, file_type: "cv", is_active: true });
+    const record = await FileModel.findOne({ token, file_type: 'cv', is_active: true });
 
     if (!record) {
       reply.STATUS = Status.NOT_FOUND;
@@ -56,14 +56,20 @@ class FilesController {
       return response.status(HTTP_STATUS_CODES.NOT_FOUND).json(reply);
     }
 
-    const list = await FileModel.find({ userId, file_type: "cv" });
+    const list = await FileModel.find({ userId, file_type: 'cv' });
 
     if (list.length) {
       list[list.length - 1].is_active = false;
       await list[list.length - 1].save();
     }
 
-    const record = await FileModel.create({ url, userId, file_type: "CV", token: user.tokens[user.tokens?.length - 1], is_active: true });
+    const record = await FileModel.create({
+      url,
+      userId,
+      file_type: 'CV',
+      token: user.tokens[user.tokens?.length - 1],
+      is_active: true,
+    });
 
     if (!record) {
       reply.STATUS = Status.ERROR;
@@ -110,7 +116,7 @@ class FilesController {
   public static async downloadCVAdmin(request: Request, response: Response) {
     const reply = new ApiResponse();
     const userId = request.query.userId;
-    const record = await FileModel.findOne({ userId, file_type: "cv", is_active: true });
+    const record = await FileModel.findOne({ userId, file_type: 'cv', is_active: true });
 
     if (!record) {
       reply.STATUS = Status.NOT_FOUND;
@@ -137,10 +143,14 @@ class FilesController {
 
     let match: Record<string, boolean> = {};
 
-    if (type) match = { is_active: true }
-    else match = { is_active: false }
+    if (type) match = { is_active: true };
+    else match = { is_active: false };
 
-    const record = await FileModel.find({ userId, file_type: "cv", ...match }, { url: 1, is_active: 1, file_type: 1 }, { lean: true });
+    const record = await FileModel.find(
+      { userId, file_type: 'cv', ...match },
+      { url: 1, is_active: 1, file_type: 1 },
+      { lean: true }
+    );
 
     reply.STATUS = Status.SUCCESS;
     reply.MESSAGE = 'CV list fetched successfully';
@@ -155,7 +165,11 @@ class FilesController {
     const reply = new ApiResponse();
     const userId = request.authenticatedUser._id;
 
-    const record = await FileModel.find({ userId, file_type: "cv" }, { url: 1, is_active: 1, file_type: 1 }, { lean: true });
+    const record = await FileModel.find(
+      { userId, file_type: 'cv' },
+      { url: 1, is_active: 1, file_type: 1 },
+      { lean: true }
+    );
 
     reply.STATUS = Status.SUCCESS;
     reply.MESSAGE = 'CV list fetched successfully';
